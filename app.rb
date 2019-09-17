b3c41a05464cc0aa86e3ed5ad8099b6944d1a79c
Rehman Ali <rehmanali001@gmail.com>
2019-09-17 18:33:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
+require 'securerandom'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end 
+    
+  get '/' do 
+  erb :index
+  end  
+  
+  post '/checkout' do 
+  @session = session
+  
+  params.each do |key, value|
+  @session[key] = value
+  end
+  end
 end
